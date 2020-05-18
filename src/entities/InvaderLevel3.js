e63b120f815f35b259fe94ca9eccf7a514d0029b
marchewjapko <57794503+marchewjapko@users.noreply.github.com>
2020-05-19 01:19:23 +0200

    deleting all files from branch

--- src/entities/InvaderLevel3.js
+++
@@ -1,9 +0,0 @@
-export default class InvaderLevel3 extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y) {
-      super(scene, x, y, "invaderLevel3"); //w tym konstruktorze dajemy nazwe do obrazka
-      this.shootInterval = Phaser.Math.Between(4000, 8000); //4000 - 8000
-      this.scene = scene;
-      this.shootTime = Phaser.Math.Between(4000, 8000);
-      this.invaderhealth = 1;
-    }
-  }

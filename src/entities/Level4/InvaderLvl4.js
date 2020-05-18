9f0bc6ddccfb24443f15dc04ecd56c398b471961
marchewjapko <57794503+marchewjapko@users.noreply.github.com>
2020-05-19 01:01:22 +0200

    so help me god

--- src/entities/Level4/InvaderLvl4.js
+++
@@ -1,9 +0,0 @@
-export default class Invader4 extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y) {
-      super(scene, x, y, "invader4"); //w tym konstruktorze dajemy nazwe do obrazka
-      this.shootInterval = Phaser.Math.Between(500, 1000);
-      this.scene = scene;
-      this.shootTime = Phaser.Math.Between(500, 1000);
-    }
-  }
-  
--- src/entities/Level5/Shield.js
+++
@@ -1,7 +0,0 @@
-export default class Shield extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y) {
-      super(scene, x, y, "shield"); //w tym konstruktorze dajemy nazwe do obrazka
-      this.scene = scene;
-    }
-  }
-  

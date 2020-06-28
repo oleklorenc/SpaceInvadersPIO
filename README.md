0ccab56a2440c69b06dd7625828367f742802c0e
olek <oleklorenc2000@gmail.com>
2020-06-28 20:28:10 +0200

    Removed files

--- README.md
+++
@@ -1,4 +0,0 @@
-# SpaceInvaders-PIO
-npm install to install dependencies
-npm start to start development mode
-npm run build to build for production
--- src/entities/Level7/InvaderLvl7.js
+++
@@ -1,9 +0,0 @@
-export default class Invader7 extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y) {
-      super(scene, x, y, "invader7"); //w tym konstruktorze dajemy nazwe do obrazka
-      this.shootInterval = Phaser.Math.Between(200, 500);
-      this.scene = scene;
-      this.shootTime = Phaser.Math.Between(200, 500);
-    }
-  }
-  

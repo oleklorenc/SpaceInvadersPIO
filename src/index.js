21f5c1259b94a9a4301f32b1ec5349d86bed20ce
olek <oleklorenc2000@gmail.com>
2020-06-28 19:57:49 +0200

    Added scale manager, final version

--- src/index.js
+++ src/index.js
@@ -21,6 +21,13 @@ import RexUIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin.js';
 
 const config = {
 	type: Phaser.AUTO,
+	scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'phaser-example',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        //width: 800,
+        //height: 600
+    },
 	width: window.innerWidth,
 	height: window.innerHeight,
 	physics: {
